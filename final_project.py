5f3ccd90dbb830a53b3c7970e60e9799954a8075
Aiman Fatima <68457614+aimanfatima1@users.noreply.github.com>
2020-07-21 00:09:04 +0300

    Trying out wordnet

--- final_project.py
+++ final_project.py
@@ -1,11 +1,18 @@
 import tkinter as tk
-
+from nltk.corpus import wordnet 
+    
 h=500
 w=600
 root = tk.Tk()
-
-def test_function():
-    print("Button clicker")
+# syns = wordnet.synsets(entry.get())
+# def test_function():
+#     # syns = wordnet.synsets(entry.get())
+#     # print(syns[0].name())
+#     print(syns[0].lemmas()[0].name())
+#     print(syns[0].definition())
+    
+# def click():
+#     entered_text=entry.get()
     
 canvas = tk.Canvas(root,height=h,width=w)
 canvas.pack()
@@ -13,3 +20,32 @@ canvas.pack()
 background_image = tk.PhotoImage(file='library.png')
 background_label = tk.Label(root,image=background_image)
 background_label.place(relwidth=1,relheight=1)
+
+frame = tk.Frame(root,bg='gray',bd=5)
+frame.place(relx=0.5,rely=0.1,relwidth=0.75,relheight=0.1,anchor='n')
+
+entry = tk.Entry(frame,font=40)
+entry.place(relwidth=0.65,relheight=1)
+
+# syns = wordnet.synsets(entry.get())
+def test_function():
+    syns = wordnet.synsets(entry.get())
+    # print(syns[0].name())
+    print(syns.name())
+    # print(syns[0].definition())
+    
+button = tk.Button(frame, text="Enter",font=40,command=test_function())
+button.place(relx=0.7,relheight=1,relwidth=0.3)  
+               
+# button = tk.Button(frame,text="enter",width=6,command=click()) 
+
+
+
+
+lower_frame = tk.Frame(root,bg='grey',bd=10)
+lower_frame.place(relx=0.5,rely=0.25,relwidth=0.75,relheight=0.6,anchor='n')
+
+label = tk.Label(lower_frame,bg='gray')
+label.place(relwidth=1,relheight=1)
+
+root.mainloop()
